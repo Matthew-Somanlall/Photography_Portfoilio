b8790a6212ef3c4436fec3a32d0eedffd268ffb9
Matthew-Somanlall <furious123.destroyer132@gmail.com>
2024-09-05 22:04:26 -0400

    V2.1 WIP
    Added a contact page, added photos and replaced others

--- Website/app.py
+++ Website/app.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
- 
